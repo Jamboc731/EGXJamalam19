e7849d07283fe7747cf85f56481a20ab751137a3
JakeusMeakus <jm219785@falmouth.ac.uk>
2019-10-18 19:56:02 +0100

    main menu, options, how to play incuded

---
+++ ManGolfSpaceBang/Assets/Scripts/UISceneScripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncromously(sceneIndex));
+    }
+
+    IEnumerator LoadAsyncromously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadScenesAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
+}
