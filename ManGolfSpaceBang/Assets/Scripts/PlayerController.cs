09e487923be59f6cfb03f00ce2abdfb1b220b57b
Jamboc731 <jamiec.covellj@gmail.com>
2019-10-18 19:17:53 +0100

    CC updates
    character controller done for both characters. the players can rotate around and "fire" their equipped weapon. Weapons just need to be properly implemented

--- ManGolfSpaceBang/Assets/Scripts/PlayerController.cs
+++ ManGolfSpaceBang/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    
 
+    int fartsInChamber = 3;
+
+    [Tooltip("The force exerted on the player when they do a cute lil toot in their soot")]
+    [SerializeField] float tootForce;
+
+    Rigidbody2D rb;
+
+    float xIn;
+
+    [Tooltip("The rotational torque applied to the player")]
+    [SerializeField] float rotT;
+
+    [Tooltip("What player number is this?")]
+    [SerializeField] char playerNo;
+
+    string hor;
+    string fire;
+
+    Weapon equipped;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        hor = string.Concat(playerNo, "Horizontal");
+        fire = string.Concat(playerNo, "Fire");
+
+    }
+
+    private void Update()
+    {
+        xIn = Input.GetAxisRaw(hor);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddTorque(-xIn * rotT);
+    }
+
+    public void Fire()
+    {
+        Debug.Log("player " + playerNo + " fired");
+
+        if (equipped != null) equipped.Fire(); 
+    }
+
+    public void PickUp(Weapon pickedUp)
+    {
+        equipped = pickedUp;
+    }
 
 }
---
+++ ManGolfSpaceBang/Assets/Scripts/PunchyBoi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchyBoi : Weapon
+{
+
+    public override void Fire()
+    {
+
+        base.Fire();
+
+
+
+    }
+
+}
