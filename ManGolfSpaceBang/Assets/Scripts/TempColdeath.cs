3f684327cbdca37cb7a73daf8b6c471ce55e4084
JakeusMeakus <jm219785@falmouth.ac.uk>
2019-10-19 11:49:12 +0100

    Update TempColdeath.cs
    added player respawning and lives calculaion

--- ManGolfSpaceBang/Assets/Scripts/TempColdeath.cs
+++ ManGolfSpaceBang/Assets/Scripts/TempColdeath.cs
@@ -6,12 +6,22 @@ using UnityEngine.SceneManagement;
 public class TempColdeath : MonoBehaviour
 {
     //meant to be temp for black hole enter 
+    public GameObject[] playerSpawns;
+    int[] playerLives = {0, 0};
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
-        Debug.Log("Player fell into the hole");
+        if (other.gameObject.tag == "Player1")
+        {
+            if(playerLives[0] < 3)
+                other.gameObject.transform.position = playerSpawns[0].transform.position;
+            playerLives[0]++;
+        }
+        else if (other.gameObject.tag == "Player2")
+        {
+            other.gameObject.transform.position = playerSpawns[1].transform.position;
+            playerLives[1]++;
+        }
     }
 
 }
